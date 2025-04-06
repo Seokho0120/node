c301962a3a2bad247790fafa11839c0d4d266597
Seokho <seokho0120@gmail.com>
2025-04-06 18:34:39 +0900

    debug

---
+++ debug/app.js
@@ -0,0 +1,20 @@
+function sayHello() {
+  console.log("hi");
+}
+
+function calculate(x, y) {
+  console.log("계산중");
+  const result = x + y;
+  console.log("result", result);
+  sayHello();
+  return result;
+}
+
+calculate(1, 2);
+
+const stop = 4;
+console.log("loading...");
+for (let i = 0; i < 10; i++) {
+  console.log("count", 1);
+  if (i === stop) break;
+}
