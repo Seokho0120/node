9bcd8afc3e9da9895bdf426369b9283b1c616658
Seokho <seokho0120@gmail.com>
2025-04-20 23:18:04 +0900

    feat: 유용한 내부 미들웨어들

--- 6-express/app-route.js
+++ 6-express/app-route.js
@@ -2,8 +2,21 @@ import express from "express";
 import postRouter from "./router/post.js";
 import useRouter from "./router/user.js";
 
+const options = {
+  dotfiles: "ignore",
+  etag: false,
+  index: false,
+  maxAge: "1d",
+  redirect: false,
+  setHeaders: function (res, req, stat) {
+    res.set("x-timestamp", Date.now());
+  },
+};
+
 const app = express();
-app.use(express.json());
+app.use(express.json()); // REST API -> Body 를 파싱할때 간편하게 사용
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body
+app.use(express.static("public", options));
 
 app.use("/posts", postRouter);
 app.use("/users", useRouter);
