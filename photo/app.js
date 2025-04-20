5d28c47c4e4be9647b5ce116e70cc71e8517f70d
Seokho <seokho0120@gmail.com>
2025-04-20 14:21:57 +0900

    feat: 메인 로직 골격 만들기

--- photo/app.js
+++ photo/app.js
@@ -4,17 +4,48 @@ const fs = require("fs");
 
 // 계획
 // 1. 사용자가 원한느 폴더의 이름을 받아온다
-// 2. 그 폴더안에 video, captured, duplicated 폴더를 만든다
-// 3. 폴더안에 있는 파일들을 다 돌면서 해당하는 mp4/mov,png/aae,IMG_1234
-
 console.log(process.argv[2]);
 const folder = process.argv[2];
 const workingDir = path.join(os.homedir(), "Pictures", folder);
-console.log("workingDir", workingDir);
 
 if (!folder || !fs.existsSync(workingDir)) {
   console.error("Please enter folder name in Pictures");
   return;
 }
 
-console.log("workingDir", workingDir);
+// 2. 그 폴더안에 video, captured, duplicated 폴더를 만든다
+const videoDir = path.join(workingDir, "video");
+const capturedDir = path.join(workingDir, "captured");
+const duplicatedDir = path.join(workingDir, "duplicated");
+
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
+
+// 3. 폴더안에 있는 파일들을 다 돌면서 해당하는 mp4/mov,png/aae,IMG_1234
+fs.promises
+  .readdir(workingDir)
+  .then((files) => processFiles(files))
+  .catch(console.log);
+
+function processFiles(files) {
+  files.forEach((file) => {
+    if (isVideoFile(file)) {
+      console.log("video", file);
+    } else if (isCapturedFile(file)) {
+      console.log("captured", file);
+    } else if (isDuplicatedFile(file)) {
+      console.log("duplicated", file);
+    }
+  });
+}
+
+function isVideoFile(file) {
+  return true;
+}
+function isCapturedFile(file) {
+  return true;
+}
+function isDuplicatedFile(file) {
+  return true;
+}
