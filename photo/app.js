176dc660247617caed8af410d1a55989155ff079
Seokho <seokho0120@gmail.com>
2025-04-20 14:35:09 +0900

    feat: 세부기능 구현

--- photo/app.js
+++ photo/app.js
@@ -31,21 +31,43 @@ fs.promises
 function processFiles(files) {
   files.forEach((file) => {
     if (isVideoFile(file)) {
-      console.log("video", file);
+      move(file, videoDir);
     } else if (isCapturedFile(file)) {
-      console.log("captured", file);
-    } else if (isDuplicatedFile(file)) {
-      console.log("duplicated", file);
+      move(file, capturedDir);
+    } else if (isDuplicatedFile(files, file)) {
+      move(file, duplicatedDir);
     }
   });
 }
 
 function isVideoFile(file) {
-  return true;
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  return !!match;
 }
+
 function isCapturedFile(file) {
-  return true;
+  const regExp = /(png|aee)$/gm;
+  const match = file.match(regExp);
+  return !!match;
 }
-function isDuplicatedFile(file) {
-  return true;
+
+// IMG_XXXX -> IMG_EXXX
+function isDuplicatedFile(files, file) {
+  if (!file.startsWith("IMG_") || file.startsWith("IMG_E")) {
+    return false;
+  }
+
+  const edited = `IMG_E${file.split("_")[1]}`;
+  const found = files.find((f) => f.includes(edited));
+  return !!found;
+}
+
+function move(file, targetDir) {
+  console.info(`${file}을 ${targetDir}으로 옮김`);
+
+  const oldPath = path.join(workingDir, file);
+  const newPath = path.join(targetDir, file);
+
+  fs.promises.rename(oldPath, newPath).catch(console.error);
 }
