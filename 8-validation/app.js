7434996e816ad870ffde9f6556e925da3675f59c
Seokho <seokho0120@gmail.com>
2025-04-27 21:26:57 +0900

    feat: validation 코드 리팩토링

--- 8-validation/app.js
+++ 8-validation/app.js
@@ -4,6 +4,14 @@ import { body, validationResult, param } from "express-validator";
 const app = express();
 app.use(express.json());
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg });
+};
+
 app.post(
   "/users",
   [
@@ -11,12 +19,9 @@ app.post(
     body("age").isInt().withMessage("숫자를 입력해!!"),
     body("email").isEmail().withMessage("이메일을 입력해!!"),
     body("job.name").notEmpty(),
+    validate,
   ],
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
     console.log(req.body);
     res.sendStatus(201);
   }
@@ -24,12 +29,9 @@ app.post(
 
 app.get(
   "/:email",
-  param("email").isEmail().withMessage("이메일을 입력해요~"),
+  [param("email").isEmail().withMessage("이메일을 입력해요~"), validate],
+
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
     res.send("📧");
   }
 );
