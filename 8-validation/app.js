efe326e515c5a1bbe335f202787215b9ed68c922
Seokho <seokho0120@gmail.com>
2025-04-27 21:01:29 +0900

    feat: express validator 사용

---
+++ 8-validation/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { body, validationResult, param } from "express-validator";
+
+const app = express();
+app.use(express.json());
+
+app.post(
+  "/users",
+  [
+    body("name").isLength({ min: 2 }).withMessage("이름은 2글자 이상!"),
+    body("age").isInt().withMessage("숫자를 입력해!!"),
+    body("email").isEmail().withMessage("이메일을 입력해!!"),
+    body("job.name").notEmpty(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    console.log(req.body);
+    res.sendStatus(201);
+  }
+);
+
+app.get(
+  "/:email",
+  param("email").isEmail().withMessage("이메일을 입력해요~"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+    res.send("📧");
+  }
+);
+
+app.listen(8080);
